86919f427ff5fe35c75917740f5e2019d09cacff
anqiwang365 <anqiwang365@gmail.com>
2020-04-19 23:41:43 -0700

    dp and revise greedy and test dp & greedy

--- src/greedy/TSPGreedy.java
+++ src/greedy/TSPGreedy.java
@@ -14,15 +14,16 @@ public class TSPGreedy {
 	private City startCity;
 	private City[] route; //store the order of visited city
 	private Set<Integer> visited;
-	private List<String> citiesSelectedUI = new ArrayList<String>();
-	private double[][] distanceMatrix;
+	private Map<Integer,City> cityInfo;//record all the city's name
+	private double[][] distanceMatrix; //record city's distance to each other
 	
 	//initialize the distance information
-	public TSPGreedy(int numberofCities,City startCity, double[][] distanceMatrix) {
+	public TSPGreedy(int numberofCities, double[][] distanceMatrix,Map<Integer,City> cityInfo,City startCity) {
 		this.numberofCities = numberofCities;
-		this.route  = new City[numberofCities];
+		this.route  = new City[numberofCities+1];
 		this.distanceMatrix = distanceMatrix;
 		this.startCity = startCity;
+		this.cityInfo = cityInfo;
 		this.visited = new HashSet<>();
 	}
 	
@@ -30,35 +31,59 @@ public class TSPGreedy {
 	 * 
 	 * @return sum of the path
 	 */
-	public double buildRoute() {
+	public double calRouteGreedy() {
 		int startId = startCity.getCityId();
-		int i=0;
+		int i=0; 
+		int index = 1; //index of route which record the order of city
 		route[0] = startCity;
 		visited.add(startId);
 		double sum = 0;
-		
 		while(i<numberofCities) {
 			double[] distance = distanceMatrix[startId];
 			double tempMin = Double.MAX_VALUE;
 			int tempId = 0;
+			//each step find the minimal distance away from the last city
 			for(int j=0;j<numberofCities;j++) {
-				if(visited.contains(j)) {
+				//if visited or the same city, then ignore
+				if(visited.contains(j)||j==tempId||j==startCity.getCityId()) {
 					continue;
 				}
-				
-				if(tempMin>distance[j]) {
+				//update minimal distance
+				if(tempMin > distance[j]) {
 					tempMin = distance[j];
 					tempId = j;
 				}
+				
 			}
-			sum += tempMin;
-			route[i] = new City(citiesSelectedUI.get(tempId),tempId,true);
+			
+			
+			if(tempMin!=Double.MAX_VALUE) {
+				sum += tempMin;
+				startId = tempId; 
+			}
+			
+			//update route in each step
+			route[index] = new City(cityInfo.get(tempId).getCityName(),tempId,true);
 			visited.add(tempId);
+			index++;
 			i++;
+			
 		}
+		
+		sum += distanceMatrix[startId][startCity.getCityId()];
 		
 		return sum;
 	}
 	
+	public String buildRoute() {
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<=numberofCities;i++) {
+			sb.append(route[i].getCityName());
+			sb.append(" - ");
+		}
+	
+		return sb.toString();
+	}
 	
 }
